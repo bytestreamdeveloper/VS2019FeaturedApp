11adc6a3a91de3aa0e04818078000244f57a3d4e
sonusathyadas <sonusathyadas@hotmail.com>
2020-08-20 10:49:26 +0530

    init

---
+++ VS2019FeaturedApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VS2019FeaturedApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
